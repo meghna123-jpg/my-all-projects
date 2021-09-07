cdc85f2f0b0427947e9018a36d469c183abeb886
Meghna <mbhattacharya296@gmail.com>
2021-09-07 22:38:21 +0530

    my all projects

---
+++ Calculator/index.js
@@ -0,0 +1,18 @@
+let result= document.getElementById("inputtext");
+ let calculate=(num)=>{
+    result.value+=num;
+ }
+ let Result=()=>{
+     try{
+         result.value=eval(result.value);
+     }
+     catch(err){
+         alert("Enter valid input");
+     }
+ }
+ function clr(){
+     result.value="";
+ }
+ function del(){
+     result.value= result.value.slice(0,-1);
+ }
